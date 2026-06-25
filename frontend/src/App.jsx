8feb5cdcd33f95a708e5ee154e1c776778424869
vignesh1445 <vigneshwarduraisamy@gmail.com>
2026-06-25 11:14:24 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
 
-  const APP_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  
   useEffect(() => {
     getStudents();
   }, []);
